6a00ae2b087f09fb223206cf56f4c0d814f4e3d3
Brandon Young <brandonyoung4d@gmail.com>
2016-10-21 10:16:49 -0700

    Remove accidental build artifacts. - A lot of build artifacts were accidentally added in the last commit.

--- BlackBoxMathLibrary/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/brandonyoung/Projects/BlackBoxMath/ToDoCore
-# Build directory: /Users/brandonyoung/Projects/BlackBoxMath/ToDoCore
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(unit_test)
