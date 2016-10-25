344a3d4d348cc236d14ff72e2e93b38e9914913d
Brandon Young <brandonyoung4d@gmail.com>
2016-10-25 12:36:39 -0700

    https://bitbucket.org/brandonyoung4d/blackboxmath/issues/3/add-using-shortcuts-for-vector-classes

--- Source/Vector.hpp
+++ Source/Vector.hpp
@@ -119,6 +119,17 @@ namespace bbm // = BlackBoxMath
             Vector<T, 3> rgb;
         };
     };
+
+    // Usings for the Vector class to make it more compact to write code.
+    using Vec2 = Vector<float, 2>;
+    using Vec3 = Vector<float, 3>;
+    using Vec4 = Vector<float, 4>;
+    using Vec2f = Vector<float, 2>;
+    using Vec3f = Vector<float, 3>;
+    using Vec4f = Vector<float, 4>;
+    using Vec2d = Vector<double, 2>;
+    using Vec3d = Vector<double, 3>;
+    using Vec4d = Vector<double, 4>;
 }
 
 #endif // VECTOR_H
